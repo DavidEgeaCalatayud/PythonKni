5926bea188e029af7b7c898853ded8ec520957e2
DavidEgea <114010484+DavidEgeaCalatayud@users.noreply.github.com>
2026-08-23 19:12:50 +0200

    Preserve Startup Manager adapter Tool export

--- tools/startup_manager_tool.py
+++ tools/startup_manager_tool.py
@@ -5,3 +5,4 @@ New code should import from :mod:`pythonkni.startup`.
 
 from pythonkni.startup.models import *  # noqa: F403
 from pythonkni.startup.service import *  # noqa: F403
+from pythonkni.startup.window import Tool as Tool
