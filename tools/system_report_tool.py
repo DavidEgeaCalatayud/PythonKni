1a90c2db826e9ff768821fb317782ffa4081ea3b
DavidEgea <114010484+DavidEgeaCalatayud@users.noreply.github.com>
2026-08-23 19:12:57 +0200

    Preserve System Report adapter exports

--- tools/system_report_tool.py
+++ tools/system_report_tool.py
@@ -5,3 +5,5 @@ New code should import from :mod:`pythonkni.system_report`.
 
 from pythonkni.system_report.models import *  # noqa: F403
 from pythonkni.system_report.service import *  # noqa: F403
+from pythonkni.system_report.window import ReportWorker as ReportWorker
+from pythonkni.system_report.window import Tool as Tool
