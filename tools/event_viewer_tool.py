fa180fc0f5364f6315b999616f3aab93e9dbc4ac
DavidEgea <114010484+DavidEgeaCalatayud@users.noreply.github.com>
2026-08-23 19:09:41 +0200

    Document legacy wildcard compatibility exports

--- tools/event_viewer_tool.py
+++ tools/event_viewer_tool.py
@@ -3,5 +3,5 @@
 New code should import from :mod:`pythonkni.event_viewer`.
 """
 
-from pythonkni.event_viewer.models import *
-from pythonkni.event_viewer.service import *
+from pythonkni.event_viewer.models import *  # noqa: F403 - legacy compatibility re-export
+from pythonkni.event_viewer.service import *  # noqa: F403 - legacy compatibility re-export
