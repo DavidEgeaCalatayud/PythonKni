3121ec31feb3013b9b3f6e84b691df57a5909460
DavidEgea <114010484+DavidEgeaCalatayud@users.noreply.github.com>
2026-09-02 10:50:21 +0200

    Validate bundled IEEE OUI registry integration

--- tests/test_network_intelligence_oui.py
+++ tests/test_network_intelligence_oui.py
@@ -7,6 +7,7 @@ from pythonkni.network_intelligence.oui import (
     lookup_mac_vendor,
     normalize_mac,
 )
+from scripts.update_oui_registry import DEFAULT_METADATA_PATH, validate_bundled_registry
 
 
 def test_normalize_mac_accepts_common_eui48_notations():
@@ -30,16 +31,31 @@ def test_randomized_multicast_and_broadcast_addresses_are_not_attributed():
     assert lookup_mac_vendor("02:11:32:AA:BB:CC") is None
 
 
-def test_bundled_registry_resolves_priority_network_vendors_offline():
+def test_bundled_ieee_registry_and_metadata_validate_offline():
     assert DEFAULT_OUI_REGISTRY.is_file()
+    assert DEFAULT_METADATA_PATH.is_file()
+    assert validate_bundled_registry() >= 20_000
+
+
+def test_bundled_registry_resolves_priority_network_vendors_offline():
     registry = load_oui_registry()
-    assert len(registry) >= 100
-    assert lookup_mac_vendor("00:11:32:AA:BB:CC") == "Synology"
-    assert lookup_mac_vendor("24:5E:BE:00:00:01") == "QNAP"
-    assert lookup_mac_vendor("0C:75:D2:12:34:56") == "Hikvision"
-    assert lookup_mac_vendor("EC:71:DB:12:34:56") == "Reolink"
-    assert lookup_mac_vendor("F0:9F:C2:12:34:56") == "Ubiquiti"
-    assert lookup_mac_vendor("B8:27:EB:12:34:56") == "Raspberry Pi"
+    assert len(registry) >= 20_000
+    assert lookup_mac_vendor("00:11:32:AA:BB:CC") == "Synology Incorporated"
+    assert lookup_mac_vendor("24:5E:BE:00:00:01") == "QNAP Systems, Inc."
+    assert (
+        lookup_mac_vendor("0C:75:D2:12:34:56")
+        == "Hangzhou Hikvision Digital Technology Co.,Ltd."
+    )
+    assert lookup_mac_vendor("EC:71:DB:12:34:56") == "Reolink Innovation Limited"
+    assert lookup_mac_vendor("F0:9F:C2:12:34:56") == "Ubiquiti Inc"
+    assert lookup_mac_vendor("B8:27:EB:12:34:56") == "Raspberry Pi Foundation"
+
+
+def test_known_ieee_duplicate_is_exposed_as_ambiguous_not_silently_overwritten():
+    assert (
+        lookup_mac_vendor("08:00:30:12:34:56")
+        == "CERN / NETWORK RESEARCH CORPORATION / ROYAL MELBOURNE INST OF TECH"
+    )
 
 
 def test_custom_registry_is_supported_without_external_lookups():
